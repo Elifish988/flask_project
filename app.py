676c14a4d7fae834edab5297ed63892d02ad58ad
Eli <elifish607@gmail.com>
2024-09-19 15:05:40 +0300

    fix bug of null values

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import os
 from flask import Flask
 from db import db
@@ -6,7 +7,7 @@ from servisses.load_players import load_players_start, add_players_to_db
 app = Flask(__name__)
 
 # יצירת הדאטה בייס
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///actors.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///players.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db_file = 'actors.db'
 
@@ -15,7 +16,10 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
-    players = load_players_start()
+    # players = load_players_start()
+    with open('nba_players_data.json', 'r') as f:
+        players = json.load(f)
+        add_players_to_db(players)
 
 
 
--- servisses/load_players.py
+++ servisses/load_players.py
@@ -9,18 +9,23 @@ def load_players_start():
     a = load_players("2022")
     b = load_players('2023')
     c = load_players('2024')
-
+    # קורא לפונקציה שמכניסה לדאטה בייס
     add_players_to_db(a + b + c)
 
 # מקבל מידע מהגסון
 def load_players(year):
     url = f"http://b8c40s8.143.198.70.30.sslip.io/api/PlayerDataTotals/query?season={year}&&pageSize=1000"
-    return requests.get(url).json()
+    result = requests.get(url)
+    json_data = result.json()
+    return json_data
 
 # print(json.dumps(load_players_start()[0], indent=4))
 
 
 #הכנסה לדב
+
+
+
 def add_players_to_db(players):
     for player in players:
         new_player = Player(
@@ -30,38 +35,28 @@ def add_players_to_db(players):
             position=player['position'],
             points=player['points'],
             games=player['games'],
-            twoPercent=player['twoPercent'],
-            threePercent=player['threePercent'],
-            ATR= ATR(player['assists'] , player['turnovers']),
-            PPG_Ratio= PPG_Ratio(player['points'], player['games'], players , player['position'])
-    )
+            twoPercent=player.get('twoPercent', 0.0),
+            threePercent=player.get('threePercent', 0.0),
+            ATR=ATR(player['assists'], player['turnovers']),
+            PPG_Ratio=PPG_Ratio(player['points'], player['games'], players, player['position'])
+        )
 
         db.session.add(new_player)
-
     db.session.commit()
 
-
-
-# ונקציה לחישוב ATR
+# פונקציה לחישוב ATR
 def ATR(assists, turnovers):
     if turnovers == 0:
         return 0
-    else:
-        return  assists / turnovers
+    return assists / turnovers
 
-
-# ונקציה לחישוב PPG_Ratio
-def PPG_Ratio(points, games, players , position):
-    points_per_gams =  points/games
+# פונקציה לחישוב PPG_Ratio
+def PPG_Ratio(points, games, players, position):
+    points_per_game = points / games if games > 0 else 0
     average_points = average_points_by_position(players, position)
     if average_points == 0:
         return 1
-    else:
-        return points_per_gams/average_points
-
-
-
-
+    return points_per_game / average_points
 
 # חישוב ממוצע כל השחקנים בעמדה לעונה
 def average_points_by_position(players, position):
@@ -71,6 +66,63 @@ def average_points_by_position(players, position):
     for player in players:
         if player['position'] == position:
             total_points += player['points']
+            players_in_position += player['games']
 
     average_points = total_points / players_in_position if players_in_position > 0 else 0
     return average_points
+
+
+# def add_players_to_db(players):
+#     for player in players:
+#         new_player = Player(
+#             playerId=player['playerId'],
+#             playerName=player['playerName'],
+#             team=player['team'],
+#             position=player['position'],
+#             points=player['points'],
+#             games=player['games'],
+#             twoPercent=player['twoPercent'],
+#             threePercent=player['threePercent'],
+#             ATR= ATR(player['assists'] , player['turnovers']),
+#             PPG_Ratio= PPG_Ratio(player['points'], player['games'], players , player['position'])
+#     )
+#
+#         db.session.add(new_player)
+#
+#     db.session.commit()
+#
+#
+#
+# # ונקציה לחישוב ATR
+# def ATR(assists, turnovers):
+#     if turnovers == 0:
+#         return 0
+#     else:
+#         return  assists / turnovers
+#
+#
+# # ונקציה לחישוב PPG_Ratio
+# def PPG_Ratio(points, games, players , position):
+#     points_per_gams =  points/games
+#     average_points = average_points_by_position(players, position)
+#     if average_points == 0:
+#         return 1
+#     else:
+#         return points_per_gams/average_points
+#
+#
+#
+#
+#
+# # חישוב ממוצע כל השחקנים בעמדה לעונה
+# def average_points_by_position(players, position):
+#     total_points = 0
+#     players_in_position = 0
+#
+#     for player in players:
+#         if player['position'] == position:
+#             total_points += player['points']
+#             players_in_position += player['games']
+#
+#     average_points = total_points / players_in_position if players_in_position > 0 else 0
+#     return average_points
